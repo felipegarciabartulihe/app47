bc6e87c5d65a4869c0fff0b6314b775abe2589e2
felipegarciabartulihe <felipegarciabartulihe@gmail.com>
2016-12-06 21:50:28 -0200

    Exercício 3.7 da página 41

---
+++ js/principal.js
@@ -0,0 +1,13 @@
+
+document.querySelector('#mudaLayout').addEventListener('click', function(){
+  var $mural = document.querySelector('.mural');
+
+  $mural.classList.toggle('mural--linhas');
+
+  if($mural.classList.contains('mural--linhas')){
+    this.textContent = 'Blocos';
+  }else {
+    this.textContent = 'Linhas';
+  }
+
+});
